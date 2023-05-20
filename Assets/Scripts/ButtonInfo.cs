c51b91b60b7e917496c42c545c9c2b8573b5a139
meggipro223 <88599405+meggipro223@users.noreply.github.com>
2023-05-20 21:31:59 +0200

    Making Item and Shop Manager Script

---
+++ Assets/Scripts/ButtonInfo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int itemId;
+    public Text priceText;
+    public Text quantityText;
+    public GameObject shopManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShopManager : MonoBehaviour
+{
+
+    public int[,] shopItems = new int[5, 5];
+    public float coins;
+    public Text coinsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinsText.text = "Coins:" + coins.ToString();
+
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
+
+        shopItems[2, 1] = 10;
+        shopItems[2, 2] = 20;
+        shopItems[2, 3] = 30;
+        shopItems[2, 4] = 40;
+
+        shopItems[3, 1] = 0;
+        shopItems[3, 2] = 0;
+        shopItems[3, 3] = 0;
+        shopItems[3, 4] = 0;
+
+    }
+
+    public void Buy() {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+        if (coins >= shopItems[2, ButtonRef.GetComponent <ButtonInfo>().itemId]) {
+
+            coins -= shopItems[2, ButtonRef.GetComponent <ButtonInfo>().itemId];
+            shopItems[3, ButtonRef.GetComponent <ButtonInfo>().itemId]++;
+            coinsText.text = "Coins:" + coins.ToString();
+            ButtonRef.GetComponent<ButtonInfo>().quantityText.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().itemId].ToString();
+
+        }
+}
+}
