034625f94ac942e150f58b0d93f142735adc7c33
meggipro223 <88599405+meggipro223@users.noreply.github.com>
2023-05-21 03:21:38 +0200

    SellManager script and Sell UI

--- Assets/Scripts/SellManager.cs
+++ Assets/Scripts/SellManager.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class ShopManager : MonoBehaviour
+public class SellManager : MonoBehaviour
 {
-
     public int[,] shopItems = new int[5, 5];
     public float coins;
     public TextMeshProUGUI coinsText;
@@ -20,33 +18,28 @@ public class ShopManager : MonoBehaviour
         //id
         shopItems[1, 1] = 1;
         shopItems[1, 2] = 2;
-        shopItems[1, 3] = 3;
-        shopItems[1, 4] = 4;
 
         //price
         shopItems[2, 1] = 60;
         shopItems[2, 2] = 70;
-        shopItems[2, 3] = 80;
-        shopItems[2, 4] = 35;
 
         //quantity
         shopItems[3, 1] = 0;
         shopItems[3, 2] = 0;
-        shopItems[3, 3] = 0;
-        shopItems[3, 4] = 0;
 
     }
 
-    public void Buy() {
+    public void Buy()
+    {
         GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
-        if (coins >= shopItems[2, ButtonRef.GetComponent <ButtonInfo>().itemId]) {
+        if (coins >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().itemId])
+        {
 
-            coins -= shopItems[2, ButtonRef.GetComponent <ButtonInfo>().itemId];
-            shopItems[3, ButtonRef.GetComponent <ButtonInfo>().itemId]++;
+            coins += shopItems[2, ButtonRef.GetComponent<ButtonInfo>().itemId];
+            shopItems[3, ButtonRef.GetComponent<ButtonInfo>().itemId]++;
             coinsText.text = "Coins:" + coins.ToString();
             ButtonRef.GetComponent<ButtonInfo>().quantityText.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().itemId].ToString();
 
         }
-}
-
+    }
 }
